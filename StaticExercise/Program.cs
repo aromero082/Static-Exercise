e2fb085262de142b263b891aa6fee6ab2e929a14
Aaron <git@github.com:aromero082/PizzaAlgo.git>
2021-12-30 15:19:56 -0800

    TempConverterClass

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class program
+    {
+        static void Main(string[] arg)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine("Result after conversion");
+            Console.WriteLine($"celsius {celsius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+        }
+    }
+}
